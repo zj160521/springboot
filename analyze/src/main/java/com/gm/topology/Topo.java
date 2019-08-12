ae31d194920204b553ad08e16131d785e9db49a0
zhouj <1197186211@qq.com>
2019-08-12 14:55:38 +0800

    打包修改

--- analyze/src/main/java/com/gm/topology/Topo.java
+++ analyze/src/main/java/com/gm/topology/Topo.java
@@ -1,30 +1,38 @@
 package com.gm.topology;
 
+import com.gm.topology.activity.ActivityStatTopo;
 import com.gm.topology.finance.StatementTopo;
+import com.gm.topology.gamerecord.GameRecordTopo;
 import com.gm.topology.platform.PlatformStatTopo;
 import org.apache.storm.LocalCluster;
 
 import com.gm.domain.Topology;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
 
 public class Topo {
 
 	public static void main(String[] args) {
 		//财务统计和活动统计topo
-//		Topology stat = StatementTopo.getTopology(args);
-//		Topology stat = ActivityStatTopo.getTopology(args);
-		Topology stat = PlatformStatTopo.getTopology(args);
+		Topology stat = StatementTopo.getTopology(args);
+		Topology act = ActivityStatTopo.getTopology(args);
+		Topology plat = PlatformStatTopo.getTopology(args);
 		//游戏记录的topo
-//		Topology gameRecord = GameRecordTopo.getTopology(args);
+		Topology gameRecord = GameRecordTopo.getTopology(args);
 		//LocalCluster用来将topology提交到本地模拟器运行，方便开发调试
-		LocalCluster cluster = new LocalCluster();
-		cluster.submitTopology(stat.getTopologyName(), stat.getConf(), stat.getStormTopology());
+//		LocalCluster cluster = new LocalCluster();
+//		cluster.submitTopology(stat.getTopologyName(), stat.getConf(), stat.getStormTopology());
 //		cluster.submitTopology(activityStat.getTopologyName(), activityStat.getConf(), activityStat.getStormTopology());
-//		try {
-//			StormSubmitter.submitTopology(financeStat.getTopologyName(), financeStat.getConf(), financeStat.getStormTopology());
-//			StormSubmitter.submitTopology(activityStat.getTopologyName(), activityStat.getConf(), activityStat.getStormTopology());
-//		} catch (AlreadyAliveException | InvalidTopologyException | AuthorizationException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			StormSubmitter.submitTopology(stat.getTopologyName(), stat.getConf(), stat.getStormTopology());
+			StormSubmitter.submitTopology(act.getTopologyName(), act.getConf(), act.getStormTopology());
+			StormSubmitter.submitTopology(plat.getTopologyName(), plat.getConf(), plat.getStormTopology());
+			StormSubmitter.submitTopology(gameRecord.getTopologyName(), gameRecord.getConf(), gameRecord.getStormTopology());
+		} catch (AlreadyAliveException | InvalidTopologyException | AuthorizationException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
