d2386ef547584663a7567222328494820185e889
zhouj <1197186211@qq.com>
2019-08-26 19:00:29 +0800

    gateway

---
+++ gateway/src/main/java/com/zuul/ZuulApplication.java
@@ -0,0 +1,16 @@
+package com.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+// 默认是开启了MongoDB，这里先禁用
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@EnableDiscoveryClient
+public class ZuulApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
