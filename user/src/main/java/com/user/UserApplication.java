27f6f68deef6e0f941a1de7b5e1802bd6279e327
zhouj <1197186211@qq.com>
2019-08-27 16:41:26 +0800

    user swagger

--- user/src/main/java/com/user/UserApplication.java
+++ user/src/main/java/com/user/UserApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfigurat
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 // 默认是开启了MongoDB，这里先禁用
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableSwagger2
 @EnableFeignClients(basePackages = "com.user.feign")
 public class UserApplication {
     public static void main( String[] args ) {
